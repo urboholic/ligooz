74f0e8bd464b1a0ec8e2a87453a29ac4403e9bb4
Maxime Spendrup <spendrup.design@gmail.com>
2024-07-01 20:51:38 +0200

    Made a template for the main function

---
+++ src/main.c
@@ -0,0 +1,12 @@
+#include "stdio.h"
+#include <complex.h>
+
+int main(int argc, char **argv) {
+    // load config files.
+
+    // Run command loop. 
+    lsh_loop();
+
+    // Perform any shurdown/cleanup.
+    return EXIT_SUCCESS;
+}
